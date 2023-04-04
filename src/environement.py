80bc07a8c517e06ca13fb6dad6d29b32d5a8ae61
fantasin <anthony.bod@gmail.com>
2023-04-04 21:29:28 +0200

    UPDATE environement.py pasing fasta

--- src/environement.py
+++ src/environement.py
@@ -1,4 +1,45 @@
-
+import os
 class Environement:
-    def __init__(self,path_file,type_parsing):
-        pass
+    def __init__(self,path_folder,type_parsing):
+        type_parsing = type_parsing.lower()
+        self.__dico_sequence = self.__parse(path_folder,type_parsing)
+
+
+
+
+    def __parse(self,path_folder,type_parsing):
+        """
+        Parse all the files
+        """
+        dico_sequence = {}
+        cmpt = 0
+
+        if path_folder[-1] == "/" or path_folder[-1] == "\\": # any one have this problem
+            path_folder = path_folder[:-1]
+
+        if type_parsing == "fasta":
+            for file in os.listdir(path_folder):
+                path_file = path_folder + "/" + file
+                dico_sequence[cmpt] = self.__parse_fasta(path_file)
+                cmpt += 1
+
+
+        return dico_sequence
+    def __parse_fasta(self,path):
+        """
+        Parse the fasta file
+        """
+        sequence = ""
+        file = open(path, 'r')
+        line =file.readline()
+        while line:
+            line = file.readline()
+            if line :
+                if line[-1] == "\n":
+                    line = line[:-1]
+                sequence += line
+
+        file.close()
+
+        return sequence
+
