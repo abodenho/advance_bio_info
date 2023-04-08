bad68246e3204f8b011320e19823bab0d9b656de
Aurelien Chassagne <62963252+AurelienCha@users.noreply.github.com>
2023-04-08 13:20:45 +0000

    Needleman Wunsch - MSA

--- src/needleman_wunsch.py
+++ src/needleman_wunsch.py
@@ -15,25 +15,29 @@ def _generator(string):
     for s in string:
         yield s
 
-def _compute_matrix(seq1, seq2):
+def _aligned_score(aligned_char, char):
+    return sum([score['match'] if c==char else score['mismatch'] for c in aligned_char])
+
+
+def _compute_matrix(aligned_seqs, new_seq):
     # Init score matrix (cumulative gap penality for first row and column, else 0)
-    matrix = [[(a+b)*score['gap'] if a==0 or b==0 else 0 for b in range(1+len(seq2))] for a in range(1+len(seq1))]
+    matrix = [[(a+b)*score['gap'] if a==0 or b==0 else 0 for b in range(1+len(new_seq))] for a in range(1+len(aligned_seqs[0]))]
     # Init direction matrix to keep track of the best direction
-    directions = [[(0,0) for b in range(1+len(seq2))] for a in range(1+len(seq1))]
+    directions = [[(0,0) for b in range(1+len(new_seq))] for a in range(1+len(aligned_seqs[0]))]
 
     # Fill the matrices
-    for i, a in enumerate(seq1):
-        for j, b in enumerate(seq2):
+    for i, aligned_char in enumerate(zip(*aligned_seqs)):
+        for j, char in enumerate(new_seq):
             s = [((0,-1), matrix[i][j+1] + score['gap']), # from top
                 ((-1,0), matrix[i+1][j] + score['gap']), # from left
-                ((-1,-1), matrix[i][j] + (score['match'] if a==b else score['mismatch']))] # from top-left
+                ((-1,-1), matrix[i][j] + _aligned_score(aligned_char, char))] # from top-left
             best = max(s, key=lambda x: x[1])
             matrix[i+1][j+1] = best[1]
             directions[i+1][j+1] = best[0]
     return directions
 
-def _find_path(seq1, seq2):
-    directions = _compute_matrix(seq1, seq2)
+def _find_path(aligned_seqs, new_seq):
+    directions = _compute_matrix(aligned_seqs, new_seq)
     i = len(directions[0])-1
     j = len(directions)-1
     path = []
@@ -46,7 +50,7 @@ def _find_path(seq1, seq2):
     return path
 
 def needleman_wunsch(seq1, seq2):
-    path = _find_path(seq1, seq2)
+    path = _find_path([seq1], seq2)
     seq1 = _generator(seq1)
     seq2 = _generator(seq2)
 
@@ -60,14 +64,24 @@ def needleman_wunsch(seq1, seq2):
     print(''.join(res2))
     return ''.join(res1), ''.join(res2)
 
+def needleman_wunsch_multiple(aligned_seqs, seq):
+    path = _find_path(aligned_seqs, seq)
+    seq = _generator(seq)
+
+    res = []
+    for node in path:
+        res.append(next(seq) if node[0] else '-')
+
+    print(''.join(res))
+    return ''.join(res)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit()
-    needleman_wunsch(sys.argv[1], sys.argv[2])
-else:
-    seq1 = 'GAAAAAAT' #'GATTACA'
-    seq2 = 'GAAT'#'GCATGCG'
-    a,b = needleman_wunsch(seq1, seq2)
-
-
+    if len(sys.argv) == 3:
+        needleman_wunsch(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) > 3:
+        needleman_wunsch_multiple(sys.argv[1:-1], sys.argv[-1])
+    else:
+        seq1 = 'GAAAAAAT' #'GATTACA'
+        seq2 = 'GAAT'#'GCATGCG'
+        a,b = needleman_wunsch(seq1, seq2)
