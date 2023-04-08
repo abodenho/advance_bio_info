0c8a84903befd58cf5280cf9f0e6e485028c694a
fantasin <anthony.bod@gmail.com>
2023-04-08 11:13:46 +0200

    ADD truncature mode

--- src/game.py
+++ src/game.py
@@ -1,24 +1,33 @@
-def play_game(environement,agent,NUMBER_EPISODE,NUMBER_TEST):
+def play_game(environement,agent,NUMBER_EPISODE,NUMBER_TEST,stop_to_truncated = False):
     print("Begin training")
     for episode in range(NUMBER_EPISODE):
-        finish = False
+        end = False
         observation = environement.reset()
 
         if (episode % 100 == 0):
             print("pourcentage ", round(episode / NUMBER_EPISODE *100, 3),"%")
 
-        while not finish:
+        while not end:
             choice = agent.make_choice(observation)
             old_obs = observation
             observation, reward, finish, truncated,info = environement.step(choice)
             agent.learn(old_obs, observation, choice, reward, finish)
 
+            if stop_to_truncated:
+                end = finish or truncated
+            else:
+                end = finish
+
     print("Begin testing")
     agent.epsilon = 0
     for i in range(NUMBER_TEST):
-        finish = False
+        end = False
         observation = environement.reset()
-        while not finish:
+        while not end:
             choice = agent.make_choice(observation)
             observation, reward, finish, truncated,info = environement.step(choice)
+            if stop_to_truncated:
+                end = finish or truncated
+            else:
+                end = finish
         print("TESTING : ",i, "\t | Choice list : ",info[0])
