dc4c89d793873268cf2f2a8c4ed728ae8f4283f6
fantasin <anthony.bod@gmail.com>
2023-04-08 10:23:04 +0200

    ADD all experiment

--- src/EXPERIMENTS.py
+++ src/EXPERIMENTS.py
@@ -31,12 +31,120 @@ def experiance_template(): # DO NOT RUN
 
     play_game(environement,agent,NUMBER_TRAINING_EPISODE,NUMBER_TEST_EPISODE)
 
+def experiment_1():
+    """
+    Experiment 1 : Hepatite C
+    """
+    # ----------------------------------------  INIT Environement ---------------------------------------------
+    PATH = "../Dataset/Hepatitis_C"
+    TYPE_PARSING = "fasta"
+    environement = Environement(PATH,TYPE_PARSING)
+
+    # ----------------------------------------  INIT AGENT ---------------------------------------------
+
+    NUMBER_SEQUENCE = environement.get_number_sequence()
+    NUMBER_STATE  = int((NUMBER_SEQUENCE**(NUMBER_SEQUENCE+1) -1) / (NUMBER_SEQUENCE-1))
+    GAMMA = 0.9
+    ALPHA = 0.8
+    EPSILON = 0.8
+    LIST_POSSIBLE_ACTION = [*range(NUMBER_SEQUENCE)]
+    agent = Agent_q_learning(LIST_POSSIBLE_ACTION,[NUMBER_STATE],GAMMA,ALPHA,EPSILON)
+
+    # ----------------------------------------  INIT GAME ---------------------------------------------
+
+    NUMBER_TRAINING_EPISODE = 10**4
+    NUMBER_TEST_EPISODE = 1
+
+    # ----------------------------------------  RUN experiment ---------------------------------------------
+    play_game(environement,agent,NUMBER_TRAINING_EPISODE,NUMBER_TEST_EPISODE)
+def experiment_2():
+    """
+    Experiment 1 : Papio Anubis
+    """
+    # ----------------------------------------  INIT Environement ---------------------------------------------
+    PATH = "../Dataset/Papio_Anubis"
+    TYPE_PARSING = "fasta"
+    environement = Environement(PATH,TYPE_PARSING)
+
+    # ----------------------------------------  INIT AGENT ---------------------------------------------
+
+    NUMBER_SEQUENCE = environement.get_number_sequence()
+    NUMBER_STATE  = int((NUMBER_SEQUENCE**(NUMBER_SEQUENCE+1) -1) / (NUMBER_SEQUENCE-1))
+    GAMMA = 0.9
+    ALPHA = 0.8
+    EPSILON = 0.8
+    LIST_POSSIBLE_ACTION = [*range(NUMBER_SEQUENCE)]
+    agent = Agent_q_learning(LIST_POSSIBLE_ACTION,[NUMBER_STATE],GAMMA,ALPHA,EPSILON)
+
+    # ----------------------------------------  INIT GAME ---------------------------------------------
+
+    NUMBER_TRAINING_EPISODE = 10**4
+    NUMBER_TEST_EPISODE = 1
+
+    # ----------------------------------------  RUN experiment ---------------------------------------------
+    play_game(environement,agent,NUMBER_TRAINING_EPISODE,NUMBER_TEST_EPISODE)
+
 def experiment_3():
     """
     Experiment 3 Dataset 1
     """
     # ----------------------------------------  INIT Environement ---------------------------------------------
     PATH = "../Dataset/Dataset_1.txt"
+    TYPE_PARSING = "txt"
+    environement = Environement(PATH,TYPE_PARSING)
+
+    # ----------------------------------------  INIT AGENT ---------------------------------------------
+
+    NUMBER_SEQUENCE = environement.get_number_sequence()
+    NUMBER_STATE  = int((NUMBER_SEQUENCE**(NUMBER_SEQUENCE+1) -1) / (NUMBER_SEQUENCE-1))
+    GAMMA = 0.9
+    ALPHA = 0.8
+    EPSILON = 0.8
+    LIST_POSSIBLE_ACTION = [*range(NUMBER_SEQUENCE)]
+    agent = Agent_q_learning(LIST_POSSIBLE_ACTION,[NUMBER_STATE],GAMMA,ALPHA,EPSILON)
+
+    # ----------------------------------------  INIT GAME ---------------------------------------------
+
+    NUMBER_TRAINING_EPISODE = 10**4
+    NUMBER_TEST_EPISODE = 1
+
+    # ----------------------------------------  RUN experiment ---------------------------------------------
+    play_game(environement,agent,NUMBER_TRAINING_EPISODE,NUMBER_TEST_EPISODE)
+
+def experiment_4():
+    """
+    Experiment 4 : Lemur gorilla mouse
+    """
+    # ----------------------------------------  INIT Environement ---------------------------------------------
+    PATH = "../Dataset/Lemur_gorilla_mouse.txt"
+    TYPE_PARSING = "txt"
+    environement = Environement(PATH,TYPE_PARSING)
+
+    # ----------------------------------------  INIT AGENT ---------------------------------------------
+
+    NUMBER_SEQUENCE = environement.get_number_sequence()
+    NUMBER_STATE  = int((NUMBER_SEQUENCE**(NUMBER_SEQUENCE+1) -1) / (NUMBER_SEQUENCE-1))
+    GAMMA = 0.9
+    ALPHA = 0.8
+    EPSILON = 0.8
+    LIST_POSSIBLE_ACTION = [*range(NUMBER_SEQUENCE)]
+    agent = Agent_q_learning(LIST_POSSIBLE_ACTION,[NUMBER_STATE],GAMMA,ALPHA,EPSILON)
+
+    # ----------------------------------------  INIT GAME ---------------------------------------------
+
+    NUMBER_TRAINING_EPISODE = 10**4
+    NUMBER_TEST_EPISODE = 1
+
+    # ----------------------------------------  RUN experiment ---------------------------------------------
+    play_game(environement,agent,NUMBER_TRAINING_EPISODE,NUMBER_TEST_EPISODE)
+
+
+def experiment_5():
+    """
+    Experiment 5 : Rat lemur opossum
+    """
+    # ----------------------------------------  INIT Environement ---------------------------------------------
+    PATH = "../Dataset/Rat_lemur_opossum.txt"
     TYPE_PARSING = "txt"
     environement = Environement(PATH,TYPE_PARSING)
 
