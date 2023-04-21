1ec91286f9f6f5b71f432ec92a5866a57bfb6f98
fantasin <anthony.bod@gmail.com>
2023-04-21 17:25:57 +0200

    ADD getter more info from experiment

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -80,12 +80,17 @@ def experiment_analyzer(data,SAVE_TO,stop_to = 100):
     best_score_average = data.get_best_score_average_serie()
     AL, EM, CS = get_AL(list_alignement),get_EM(list_alignement),get_CS(list_alignement)
     name = SAVE_TO + data.get_name() + "/" + data.get_name()
-    string_info = "SCORE : " +  str(score) + "\nAL : " \
-                  +  str(AL) + "\nEM : " + str(EM) + "\nCS : " + str(CS) + "\n"
+
 
     running_info = data.get_info_experiment()
+
+    number_episode_before_reach_best_score, time_to_reach_best_score  = get_best_info(time_series,best_score_average)
     with open(name + '.txt', 'w') as f:
+        string_info = "SCORE : " + str(score) + "\nAL : " \
+                      + str(AL) + "\nEM : " + str(EM) + "\nCS : " + str(CS) + "\n"
         f.write(string_info)
+        f.write("Number episode before reach best score : " + str(number_episode_before_reach_best_score))
+        f.write("Time to reach best score : " + str(time_to_reach_best_score))
         for key in running_info:
             string = key + " : "+str(running_info[key]) + "\n"
             f.write(string)
@@ -93,3 +98,14 @@ def experiment_analyzer(data,SAVE_TO,stop_to = 100):
     plot_time(time_series,name,stop_to)
     plot_score(score_average,name,stop_to)
     plot_best_score(best_score_average,name,stop_to)
+
+def get_best_info(time_series,best_score_average):
+    best_score = best_score_average[-1]
+    number_episode_to_reach_best_score = "error"
+    time_to_reach_best_score = "error"
+    for episode in range(len(best_score_average)):
+        if best_score_average[episode] == best_score:
+            number_episode_to_reach_best_score = episode + 1 # +1 because we count at 1
+            time_to_reach_best_score = time_series[episode]
+
+    return number_episode_to_reach_best_score, time_to_reach_best_score
