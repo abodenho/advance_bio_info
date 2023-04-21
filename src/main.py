df4cb9d369d558d2836f31771641a5671ba5422c
fantasin <anthony.bod@gmail.com>
2023-04-21 16:50:14 +0200

    ADD all experiment run + folder analisis

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from EXPERIMENTS import *
-from data_plotter import *
-if __name__ == "__main__":
-    data = experiment_5()
+from data_analyzer import *
+import os
 
-    STOP_TO = 60
-    plot_best_score(data.get_best_score_average_serie(),STOP_TO)
-    plot_time(data.get_time_average_serie(),STOP_TO)
+PATH_SAVE_DATA = "../data_experiment/"
+
+def run_all_experimnet(gather_data = True):
+
+    if gather_data and not os.path.isdir(PATH_SAVE_DATA):
+        os.mkdir(PATH_SAVE_DATA)
+
+    for i in range(3,6):
+        data = eval("experiment_{}()".format(i))
+        name_experimnet = data.get_name()
+        if gather_data and not os.path.isdir(PATH_SAVE_DATA + name_experimnet):
+            os.mkdir(PATH_SAVE_DATA + name_experimnet)
+
+        experiment_analyzer(data,PATH_SAVE_DATA)
+
+if __name__ == "__main__":
+    run_all_experimnet()
